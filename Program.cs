f4065b0b079c50ede121158d38bd8e9757aa1d4c
MeatTH <122563787+MeatTH@users.noreply.github.com>
2023-05-20 15:37:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        investor();
+
+    }
+    static void investor()
+    {
+       int inputN;
+        int[] N = new int[inputN = int.Parse(Console.ReadLine())];
+        int K = int.Parse(Console.ReadLine());
+
+        for(int i=0; i < inputN; i++)
+        {
+            N[i] = int.Parse(Console.ReadLine());
+        }
+
+        int count,max = 0;
+
+        for(int i = K; i < inputN-K; i++)
+        {
+            int Side = 0;
+            for(int j = 1; j < K+1; j++)
+            {
+            Side = Side + N[i-j] + N[i+j];
+            }
+            count = Side + N[i];
+            if(count>max)
+            {
+                max = count;
+            }
+        }       
+        Console.WriteLine(max);
+    }     
+}
+
+
+
+
+
+
